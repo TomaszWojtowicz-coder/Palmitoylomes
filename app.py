57e3b0bfc6e6c37b524d507f457ef7528b63559f
TomaszWojtowicz-coder <joncy1234@gmail.com>
2025-02-21 15:27:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,8 @@ def get_base64(image_path):
 sidebar_bg_image = get_base64("x2.png")  # Ensure the correct file path
 
 # Apply CSS with inline Base64 image and set text color to white
+
+
 st.markdown(f"""
     <style>
         [data-testid="stSidebar"] {{
@@ -92,13 +94,14 @@ st.markdown(f"""
             background-position: center;
         }}
 
-        /* Ensure only the "Choose a section" text is white */
+        /* Zmieniamy tylko kolor i rozmiar czcionki dla "Choose a section" */
         section[data-testid="stSidebar"] label {{
             color: white !important;
+            font-size: 30px !important; /* Powiększona czcionka */
+            font-weight: bold !important; /* Pogrubienie (opcjonalnie) */
         }}
     </style>
 """, unsafe_allow_html=True)
-
 
 
 # === SIDEBAR MENU ===
