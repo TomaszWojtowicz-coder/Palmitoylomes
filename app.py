47b1c040abfeb5b3e8f4e1fcdf7fdbefd485d615
TomaszWojtowicz-coder <joncy1234@gmail.com>
2025-02-28 14:20:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -234,7 +234,7 @@ elif page == "PROJECT DESCRIPTION":
     Main1 = resize_image("Main1.png")
     
     # Create three equal columns
-    col1, col2, col3 = st.columns([0.25, 0.25, 0.25]) 
+    spacer1, col1, col2, col3, spacer2 = st.columns([2, 0.15, 0.15, 0.15, 2])
     
     # Display title and images in each column
     with col1:
